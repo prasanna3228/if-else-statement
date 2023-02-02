c783bf8aababe90e6fdfd678f97bd4081852638f
prasanna3228 <prasannakumar3228@gmail.com>
2023-02-02 07:14:51 +0000

    using ternary operators

--- src/App.js
+++ src/App.js
@@ -9,19 +9,13 @@ class App extends Component {
     isLoggedIn: false,
   }
 
-  renderAuthButton = () => {}
-
   render() {
     const {isLoggedIn} = this.state
-    let authButton
-    if (isLoggedIn === true) {
-      authButton = <button>Lougout</button>
-    } else authButton = <button>Login</button>
 
     return (
       <div className="container">
         <Welcome greeting="Hello" name="User" />
-        {authButton}
+        {isLoggedIn ? <button>Logout</button> : <button>Login</button>}
       </div>
     )
   }
